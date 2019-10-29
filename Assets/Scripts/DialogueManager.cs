8e41bde17e206a136e08dcfa78bb8799bacf3b96
Julianna Shevchenko <juliannashevchenko@Juliannas-MacBook-Air.local>
2019-10-29 18:12:00 -0400

    text message system implemented

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -16,23 +16,37 @@ public class DialogueManager : MonoBehaviour
 
     public GameObject textBox;
     public Button continueButt;
+    public GameObject phoneObj;
+    SpriteRenderer phoneSR;
     public Dialogue dig;
 
     public Button[] toIDbuttons;
-    //toID5, toID6, toID7, toID8, toID9, toNowhere
+    // toID5 (0), toID6 (1), toID7 (2), toID8 (3), toID9 (4), toNowhere (5), toID11 (6)
+    // toID12 (7), toID13 (8), toNowhere(1) (8), toID14 (9), toID15 (10), toID16 (11), toID17 (12)
+    // toNowhere(2) (13), toID18 (14), toID19 (15), toID20 (16), toID21 (18), toNowhere(3) (19)
+    // toID22 (20), toID23 (21), toID24 (22), toID25 (23), toID26 (24), toID27 (25)
+    // toID28 (26), toID29 (27), toID30 (28), toID31 (29)
 
-	// public Animator animator;
+    public Sprite[] messageSprites;
+    // 9 Andrew, 5 Isabel = 14 (13) total
 
-	private Queue<string> sentences;
+    bool once = true;
+
+    // public Animator animator;
+
+    private Queue<string> sentences;
 
 	// Use this for initialization
 	void Start()
 	{
-		sentences = new Queue<string>();
+        disableAllButtons();
+        sentences = new Queue<string>();
         continueButt.gameObject.SetActive(false);
         textBox.SetActive(false);
         dialogueText.text = "";
         envSR = environment.GetComponent<SpriteRenderer>();
+        phoneSR = phoneObj.GetComponent<SpriteRenderer>();
+        phoneObj.SetActive(false);
     }
 
 	public void StartDialogue(Dialogue dialogue)
@@ -123,9 +137,311 @@ public class DialogueManager : MonoBehaviour
                 toIDbuttons[1].gameObject.SetActive(true);
                 toIDbuttons[3].gameObject.SetActive(true);
                 break;
+            case 10:
+                disableAllButtons();
+                continueButt.gameObject.SetActive(false);
+                // toID11, toID12, and to ID13
+                toIDbuttons[6].gameObject.SetActive(true);
+                toIDbuttons[7].gameObject.SetActive(true);
+                toIDbuttons[8].gameObject.SetActive(true);
+                break;
+            case 11:
+                disableAllButtons();
+                continueButt.gameObject.SetActive(false);
+                // toNowhere(1), toID14, and to ID15
+                toIDbuttons[9].gameObject.SetActive(true);
+                toIDbuttons[10].gameObject.SetActive(true);
+                toIDbuttons[11].gameObject.SetActive(true);
+                break;
+            case 12:
+                consequences.text = "Your symptoms have worsened...";
+                switchWorld();
+                disableAllButtons();
+                dialogueText.text = "";
+                textBox.SetActive(false);
+                Time.timeScale = 1;
+                continueButt.gameObject.SetActive(false);
+                break;
+            case 13:
+                disableAllButtons();
+                continueButt.gameObject.SetActive(false);
+                // toID16 and to ID17
+                toIDbuttons[12].gameObject.SetActive(true);
+                toIDbuttons[13].gameObject.SetActive(true);
+                break;
+
+            case 14:
+                disableAllButtons();
+                dialogueText.text = "";
+                continueButt.gameObject.SetActive(false);
+                phoneObj.SetActive(true);
+                phoneSR.sprite = messageSprites[0];
+                // toID23 and to ID24
+                toIDbuttons[21].gameObject.SetActive(true);
+                toIDbuttons[22].gameObject.SetActive(true);
+                break;
+            case 15:
+                disableAllButtons();
+                dialogueText.text = "";
+                continueButt.gameObject.SetActive(false);
+                phoneObj.SetActive(true);
+                phoneSR.sprite = messageSprites[9];
+                // toID30 (28) and toID31 (29)
+                toIDbuttons[28].gameObject.SetActive(true);
+                toIDbuttons[29].gameObject.SetActive(true);
+                break;
+
+            case 16:
+                disableAllButtons();
+                continueButt.gameObject.SetActive(false);
+                // toNowhere(3) and toID22
+                toIDbuttons[19].gameObject.SetActive(true);
+                toIDbuttons[20].gameObject.SetActive(true);
+                break;
+            case 17:
+                disableAllButtons();
+                continueButt.gameObject.SetActive(false);
+                // toNowhere(2), toID18, and to ID19
+                toIDbuttons[14].gameObject.SetActive(true);
+                toIDbuttons[15].gameObject.SetActive(true);
+                toIDbuttons[16].gameObject.SetActive(true);
+                break;
+            case 18:
+                disableAllButtons();
+                continueButt.gameObject.SetActive(false);
+                // toID20 and to ID21
+                toIDbuttons[17].gameObject.SetActive(true);
+                toIDbuttons[18].gameObject.SetActive(true);
+                break;
+            case 21:
+                consequences.text = "Your relationship with this character has weakened...";
+                switchWorld();
+                disableAllButtons();
+                dialogueText.text = "";
+                textBox.SetActive(false);
+                Time.timeScale = 1;
+                continueButt.gameObject.SetActive(false);
+                break;
+            case 22:
+                consequences.text = "Your relationship with this character has weakened..." + "\n" + "Your symptoms have worsened...";
+                switchWorld();
+                disableAllButtons();
+                dialogueText.text = "";
+                textBox.SetActive(false);
+                Time.timeScale = 1;
+                continueButt.gameObject.SetActive(false);
+                break;
+
+            case 23:
+                if (once) {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[1];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                } else
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+            case 24:
+                disableAllButtons();
+                dialogueText.text = "";
+                continueButt.gameObject.SetActive(false);
+                phoneSR.sprite = messageSprites[2];
+                // toID23 and to ID25
+                toIDbuttons[21].gameObject.SetActive(true);
+                toIDbuttons[23].gameObject.SetActive(true);
+                break;
+            case 25:
+                disableAllButtons();
+                dialogueText.text = "";
+                continueButt.gameObject.SetActive(false);
+                phoneSR.sprite = messageSprites[4];
+                // toID26 and to ID27
+                toIDbuttons[24].gameObject.SetActive(true);
+                toIDbuttons[25].gameObject.SetActive(true);
+                break;
+
+            case 26:
+                if (once)
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[5];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                }
+                else
+                {
+                    consequences.text = "Your relationship with this character has weakened...";
+                    switchWorld();
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+            case 27:
+                disableAllButtons();
+                dialogueText.text = "";
+                continueButt.gameObject.SetActive(false);
+                phoneSR.sprite = messageSprites[6];
+                // toID28 and to ID29
+                toIDbuttons[26].gameObject.SetActive(true);
+                toIDbuttons[27].gameObject.SetActive(true);
+                break;
+            case 28:
+                if (once)
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[7];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                }
+                else
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+            case 29:
+                if (once)
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[8];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                }
+                else
+                {
+                    consequences.text = "Your relationship with this character has weakened...";
+                    switchWorld();
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+            case 30:
+                if (once)
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[10];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                }
+                else
+                {
+                    consequences.text = "Your relationship with this character has weakened...";
+                    switchWorld();
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+            case 31:
+                disableAllButtons();
+                dialogueText.text = "";
+                continueButt.gameObject.SetActive(false);
+                phoneObj.SetActive(true);
+                phoneSR.sprite = messageSprites[11];
+                // toID32 (30) and toID33 (31)
+                toIDbuttons[30].gameObject.SetActive(true);
+                toIDbuttons[31].gameObject.SetActive(true);
+                break;
+            case 32:
+                if (once)
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[12];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                }
+                else
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+            case 33:
+                if (once)
+                {
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    continueButt.gameObject.SetActive(true);
+                    phoneSR.sprite = messageSprites[13];
+                    Debug.Log("Done");
+                    once = false;
+                    break;
+                }
+                else
+                {
+                    consequences.text = "Your relationship with this character has weakened...";
+                    switchWorld();
+                    disableAllButtons();
+                    dialogueText.text = "";
+                    phoneObj.SetActive(false);
+                    textBox.SetActive(false);
+                    Time.timeScale = 1;
+                    continueButt.gameObject.SetActive(false);
+                    once = true;
+                    Debug.Log(once);
+                    break;
+                }
+
             default:
                 disableAllButtons();
                 dialogueText.text = "";
+                phoneObj.SetActive(false);
                 textBox.SetActive(false);
                 Time.timeScale = 1;
                 continueButt.gameObject.SetActive(false);
@@ -148,7 +464,7 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
-    public IEnumerator StartCountdown(float countdownValue = 10)
+    public IEnumerator StartCountdown(float countdownValue = 5)
     {
         currCountdownValue = countdownValue;
         while (currCountdownValue > 0)
@@ -157,6 +473,7 @@ public class DialogueManager : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
             currCountdownValue--;
         }
+        consequences.text = "";
     }
 
 }
